ad5a05df9915f0e7317ac26a3dfeaa36207f2aed
RuanThiago <Ruanthiago67@gmail.com>
2022-07-19 08:34:13 -0300

    funcions salvar,listar e buscar feitas

--- funcoes.js
+++ funcoes.js
@@ -1,11 +1,40 @@
 const cachorros = require('./database/cachorros.json');
+let fs = require('fs')
 
-function salvar(){}
+function salvar(){
 
-function buscar(id){}
+}
+
+function buscar(id){
+    const cachorro  = cachorros.find(cachorro=>cachorro.id == id);    
+    if(!cachorro){
+        return `Não existe cachorro com o id ${id}`
+    }
+    return cachorro
+}
 
 function listar(){
-    console.table(cachorros);
+    console.table(cachorros);   
+}
+
+function descrever(){
+    
+}
+
+function adicionar(){
+    
+}
+
+function vacina(){
+
+}
+
+function atribuirServico(){
+
+}
+
+function remover(){
+    
 }
 
 let funcoes = module.exports = {
